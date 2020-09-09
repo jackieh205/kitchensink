3f9bd9d60ec2f47aafe7aee5d5702494845267a6
jackieh205 <jacquelineharper90@gmail.com>
2020-09-09 06:11:59 -0500

    new4

---
+++ app.js
@@ -0,0 +1,19 @@
+var myName = " Jackie";
+/*name variable*/
+
+const stateNumbers = '50';
+/*state constant*/
+ 
+var add = 5 + 4;
+
+var veg =['Asparagus, Zucchini, Carrots, Corn']
+
+for( var i = 0; i < 4; i++)
+{ console.log(veg)}
+alert ('Hello Worlde')
+
+console.log(myName);
+console.log(stateNumbers);
+console.log(add)
+
+
